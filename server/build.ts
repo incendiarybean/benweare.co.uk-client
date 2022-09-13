0110280df38d19b38d9562ac55a2f8a3026bfbb5
incendiarybean <ben.weare12@gmail.com>
2022-09-13 15:03:59 +0100

    Adjusted file paths

--- server/build.ts
+++ server/build.ts
@@ -4,12 +4,12 @@ import os from "os";
 console.log("Moving client files...");
 if (os.type() === "Linux") {
     exec("mv build ./dist/app");
-    exec("cp -r ./resources/audio ../dist/resources/audio", (error) => {
+    exec("cp -r ./server/resources/audio ./dist/resources/audio", (error) => {
         console.log(error);
     });
 } else if (os.type() === "Windows_NT") {
     exec("move build ./dist/app");
-    exec("copy -r ./resources/audio ../dist/resources/audio", (error) => {
+    exec("copy -r ./server/resources/audio ./dist/resources/audio", (error) => {
         if (error) {
             console.log(error);
         }
