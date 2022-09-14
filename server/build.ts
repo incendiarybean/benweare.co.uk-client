fa04e0b19232c19ca5c64e082a11525241a3b5b6
incendiarybean <ben.weare12@gmail.com>
2022-09-14 11:17:34 +0100

    Update to remove EXEC entirely

--- server/build.ts
+++ server/build.ts
@@ -1,36 +1,39 @@
-import { exec } from "child_process";
 import fs from "fs";
-import os from "os";
 import path from "path";
 
-const syncServerDist = (sourcePath: string) => {
-    const source = `./server/${sourcePath}`;
-    const destination = `./dist/${sourcePath}`;
-
-    const srcExists = fs.existsSync(source);
+const syncServerDist = (sourcePath: string, sourceDestination: string) => {
+    const srcExists = fs.existsSync(sourcePath);
     if (!srcExists) {
         throw new Error("File source doesn't exist.");
     }
 
-    const isDirectory = fs.statSync(source).isDirectory();
+    const isDirectory = fs.statSync(sourcePath).isDirectory();
     if (isDirectory) {
-        const destExists = fs.existsSync(destination);
+        const destExists = fs.existsSync(sourceDestination);
         if (!destExists) {
-            fs.mkdirSync(destination);
+            fs.mkdirSync(sourceDestination);
         }
 
-        fs.readdirSync(source).forEach(function (childItemName) {
-            syncServerDist(path.join(sourcePath, childItemName));
+        fs.readdirSync(sourcePath).forEach(function (childItemName) {
+            syncServerDist(
+                path.join(sourcePath, childItemName),
+                path.join(sourceDestination, childItemName)
+            );
         });
     } else {
-        fs.copyFileSync(source, destination);
+        console.log(`Copying file: ${sourcePath} -> ${sourceDestination}`);
+        fs.copyFileSync(sourcePath, sourceDestination);
     }
 };
 
-console.log("Moving client files...");
-if (os.type() === "Linux") {
-    exec("mv build ./dist/app");
-} else if (os.type() === "Windows_NT") {
-    exec("move build ./dist/app");
-}
-syncServerDist("resources/audio");
+const fileSync = [
+    {
+        source: "./server/resources/audio",
+        destination: "./dist/resources/audio",
+    },
+    { source: "./build", destination: "./dist/app" },
+];
+
+fileSync.forEach(({ source, destination }) =>
+    syncServerDist(source, destination)
+);
