b0e4ed0ac3a5eec4927ff1c45dacd2006e92eaa9
Ben <66177972+incendiarybean@users.noreply.github.com>
2022-10-17 10:49:31 +0100

    Revert "Bug Fixes (#5)" (#9)
    This reverts commit 6986a06dc726761c5c0634c9bbaa08055409a565.

--- src/TS/socket.ts
+++ src/TS/socket.ts
@@ -1,10 +1,8 @@
 import { io } from "socket.io-client";
 
-const { REACT_APP_HOSTNAME, REACT_APP_LOCAL, NODE_ENV } = process.env;
-const socketHostname =
-    NODE_ENV === "production" ? REACT_APP_HOSTNAME : REACT_APP_LOCAL;
+const { REACT_APP_HOSTNAME } = process.env;
 
-const IO = io(socketHostname as string);
+const IO = io(REACT_APP_HOSTNAME as string);
 
 IO.on("connect_error", (err) => {
     console.log(err.toString());
