bc1c617215463a41c60b2fb4ed50facfac75376e
incendiarybean <ben.weare12@gmail.com>
2022-10-17 10:16:32 +0100

    Dynamic variables for local builds

--- src/TS/socket.ts
+++ src/TS/socket.ts
@@ -1,8 +1,10 @@
 import { io } from "socket.io-client";
 
-const { REACT_APP_HOSTNAME } = process.env;
+const { REACT_APP_HOSTNAME, REACT_APP_LOCAL, NODE_ENV } = process.env;
+const socketHostname =
+    NODE_ENV === "production" ? REACT_APP_HOSTNAME : REACT_APP_LOCAL;
 
-const IO = io(REACT_APP_HOSTNAME as string);
+const IO = io(socketHostname as string);
 
 IO.on("connect_error", (err) => {
     console.log(err.toString());
