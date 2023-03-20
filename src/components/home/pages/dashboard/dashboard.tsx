dc9f00764c141addfd13903df06160c28cda2b28
Ben <66177972+incendiarybean@users.noreply.github.com>
2023-03-20 10:27:43 +0000

    HOTFIX - Updated dashboard order and colour of component (#32)
    * Updated Dashboard order
    
    * Fixed colours of card-component when using a video link
    
    * bumped version

--- src/components/home/pages/dashboard/dashboard.tsx
+++ src/components/home/pages/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { Card, NewsCarousel } from "src/components/";
 const DashboardPage = ({ Icon, mobileMenu }: BodyProps) => {
     return (
         <div className="select-none items-center flex flex-col w-full">
+            <Card SiteName="NASA" Endpoint={"/api/news/nasa"} />
             <NewsCarousel
                 Icon={Icon}
                 Endpoint={"/api/news/pcgamer"}
@@ -11,7 +12,6 @@ const DashboardPage = ({ Icon, mobileMenu }: BodyProps) => {
                 Disabled={mobileMenu}
             />
             <hr className="border-b border-slate-300 dark:border-slate-600 w-2/3 self-center lg:border-none" />
-            <Card SiteName="NASA" Endpoint={"/api/news/nasa"} />
             <hr className="border-b border-slate-300 dark:border-slate-600 w-2/3 self-center lg:border-none" />
             <NewsCarousel
                 Icon={Icon}
