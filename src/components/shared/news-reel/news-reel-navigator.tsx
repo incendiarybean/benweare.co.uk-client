f73289a31fafba686ccef663c7b75800c1983f9f
Benjamin Weare <ben.weare12@gmail.com>
2024-03-04 10:50:19 +0000

    Limit to 30 as latest news is latest

--- src/components/shared/news-reel/news-reel-navigator.tsx
+++ src/components/shared/news-reel/news-reel-navigator.tsx
@@ -1,10 +1,11 @@
-import type { NewsArticle } from '@common/types';
 import {
     LeftArrow,
     LeftCircleArrow,
     RightArrow,
     RightCircleArrow,
 } from '@icons';
+
+import type { NewsArticle } from '@common/types';
 
 const NewsReelNavigator = ({
     handleRotation,
@@ -22,9 +23,7 @@ const NewsReelNavigator = ({
         <div className='w-full lg:mt-2 flex justify-center'>
             <div className='hidden bg-white dark:bg-zinc-900 rounded-b lg:rounded md:flex gap-2 md:gap-0 w-full lg:w-2/3 p-3 justify-between h-12 items-center lg:shadow lg:border border-slate-300 dark:border-zinc-600/30'>
                 <button
-                    aria-label={`Return to previous Article (Article ${
-                        currentPage - 1
-                    })`}
+                    aria-label={`Return to previous Article (Article ${currentPage - 1})`}
                     className='w-full carousel-button'
                     onClick={() => handleRotation(currentPage - 1)}
                 >
@@ -32,22 +31,18 @@ const NewsReelNavigator = ({
                 </button>
 
                 {articles.map((data, index) => (
+                    index < 30 &&
                     <button
-                        aria-label={`Move to a selected Article (Article ${
-                            index + 1
-                        })`}
+                        aria-label={`Move to a selected Article (Article ${index + 1})`}
                         key={`${data.url}-${data.id}-navigator`}
                         onClick={() => handleRotation(index)}
-                        className={`carousel-pip ${
-                            index === currentPage ? 'active' : 'inactive'
-                        }`}
+                        className={`carousel-pip ${index === currentPage ? 'active' : 'inactive'}`}
                     />
                 ))}
 
                 <button
-                    aria-label={`Move to next Article (Article ${
-                        currentPage + 1
-                    })`}
+                    aria-label={`Move to next Article (Article ${currentPage + 1
+                        })`}
                     className='w-full carousel-button'
                     onClick={() => handleRotation(currentPage + 1)}
                 >
@@ -56,9 +51,7 @@ const NewsReelNavigator = ({
             </div>
             <div className='md:hidden bg-white dark:bg-zinc-900 rounded-b flex w-full p-3 justify-between h-12 items-center'>
                 <button
-                    aria-label={`Return to previous Article (Article ${
-                        currentPage - 1
-                    })`}
+                    aria-label={`Return to previous Article (Article ${currentPage - 1})`}
                     className='md:hidden default-link w-auto min-w-fit flex justify-start items-center gap-1'
                     onClick={() => handleRotation(currentPage - 1)}
                 >
@@ -66,9 +59,7 @@ const NewsReelNavigator = ({
                 </button>
 
                 <button
-                    aria-label={`Move to next Articl (Article ${
-                        currentPage + 1
-                    })`}
+                    aria-label={`Move to next Article (Article ${currentPage + 1})`}
                     className='md:hidden default-link w-auto min-w-fit flex justify-end items-center gap-1'
                     onClick={() => handleRotation(currentPage + 1)}
                 >
