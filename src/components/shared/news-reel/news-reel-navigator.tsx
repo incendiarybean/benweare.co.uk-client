58a7042d1fa5b4f0e96ae37164415caf5ef7a30c
Ben <66177972+incendiarybean@users.noreply.github.com>
2024-03-04 10:58:13 +0000

    CHANGE: Reverted new carousel controller (#47)
    * bumped version
    
    * Limit to 30 as latest news is latest
    
    * bumped version
    
    * Removed unused elements
    
    * Removed imports

--- src/components/shared/news-reel/news-reel-navigator.tsx
+++ src/components/shared/news-reel/news-reel-navigator.tsx
@@ -1,10 +1,8 @@
 import {
     LeftArrow,
     LeftCircleArrow,
-    LeftDoubleArrow,
     RightArrow,
     RightCircleArrow,
-    RightDoubleArrow,
 } from '@icons';
 
 import type { NewsArticle } from '@common/types';
@@ -25,80 +23,31 @@ const NewsReelNavigator = ({
     return (
         <div className='w-full lg:mt-2 flex justify-center'>
             <div className='hidden bg-white dark:bg-zinc-900 rounded-b lg:rounded md:flex gap-2 md:gap-0 w-full lg:w-2/3 p-3 justify-between h-12 items-center lg:shadow lg:border border-slate-300 dark:border-zinc-600/30'>
-                {articles.length >= 30 &&
-                    <button
-                        aria-label={`Return to the first Article`}
-                        className='w-full carousel-button'
-                        onClick={() => handleRotation(0)}
-                    >
-                        <LeftDoubleArrow />
-                    </button>
-                }
-
                 <button
                     aria-label={`Return to previous Article (Article ${currentPage - 1})`}
-                    className='w-full carousel-button ml-2'
+                    className='w-full carousel-button'
                     onClick={() => handleRotation(currentPage - 1)}
                 >
                     <LeftArrow />
                 </button>
 
-                <div className='w-full flex justify-around'>
-                    {articles.length < 30 && articles.map((data, index) => (
-                        <button
-                            aria-label={`Move to a selected Article (Article ${index + 1})`}
-                            key={`${data.url}-${data.id}-navigator`}
-                            onClick={() => handleRotation(index)}
-                            className={`carousel-pip ${index === currentPage ? 'active' : 'inactive'}`}
-                        />
-                    ))}
-                    {articles.length >= 30 &&
-                        <>
-                            {articles.map((data, index) => (
-                                (index < currentPage + 5 && index !== articles.length - 1) &&
-                                <button
-                                    hidden={index < currentPage - 5}
-                                    aria-label={`Move to a selected Article (Article ${index + 1})`}
-                                    key={`${data.url}-${data.id}-navigator`}
-                                    onClick={() => handleRotation(index)}
-                                    className={`${index === currentPage ? 'text-sky-400' : 'text-zinc-400'} hover:text-sky-500 dark:hover:text-sky-400`}
-                                >
-                                    {index + 1}
-                                </button>
-                            ))}
-                            {currentPage + 6 <= articles.length &&
-                                <span>...</span>
-                            }
-                            <button
-                                aria-label={`Move to a selected Article (Article ${articles.length - 1})`}
-                                key={`${articles[articles.length - 1].url}-${articles[articles.length - 1].id}-navigator`}
-                                onClick={() => handleRotation(articles.length - 1)}
-                                className={`${articles.length - 1 === currentPage ? 'text-sky-400' : 'text-zinc-400'} hover:text-sky-500 dark:hover:text-sky-400`}
-                            >
-                                {articles.length}
-                            </button>
-
-                        </>
-                    }
-                </div>
+                {articles.map((data, index) => (
+                    index < 30 &&
+                    <button
+                        aria-label={`Move to a selected Article (Article ${index + 1})`}
+                        key={`${data.url}-${data.id}-navigator`}
+                        onClick={() => handleRotation(index)}
+                        className={`carousel-pip ${index === currentPage ? 'active' : 'inactive'}`}
+                    />
+                ))}
 
                 <button
                     aria-label={`Move to next Article (Article ${currentPage + 1})`}
-                    className='w-full carousel-button mr-2'
+                    className='w-full carousel-button'
                     onClick={() => handleRotation(currentPage + 1)}
                 >
                     <RightArrow />
                 </button>
-
-                {articles.length >= 30 &&
-                    <button
-                        aria-label={`Move to the last Article`}
-                        className='w-full carousel-button'
-                        onClick={() => handleRotation(articles.length - 1)}
-                    >
-                        <RightDoubleArrow />
-                    </button>
-                }
             </div>
             <div className='md:hidden bg-white dark:bg-zinc-900 rounded-b flex w-full p-3 justify-between h-12 items-center'>
                 <button
