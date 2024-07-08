1c551c9a38fa21d7b075d63193cd994e3c8edcb6
Benjamin Weare <ben.weare12@gmail.com>
2024-07-08 10:24:35 +0100

    Removed untestable

--- src/components/shared/footer/footer-component.cy.tsx
+++ src/components/shared/footer/footer-component.cy.tsx
@@ -1,23 +1,9 @@
 import '../../../index.css';
 
-import * as pkg from 'package.json';
-
 import FooterComponent from './footer-component';
 
 describe('<FooterComponent />', () => {
     it('should render without any props', () => {
         cy.mount(<FooterComponent />);
-    });
-
-    it('should render with the correct version number', () => {
-        cy.mount(<FooterComponent />);
-
-        const package_version =
-            VITE_APP_VERSION ?? process.env?.VITE_APP_VERSION ?? pkg.version;
-
-        cy.get('[data-cy="version-number"]').should(
-            'contain.text',
-            `v${package_version}`
-        );
     });
 });
