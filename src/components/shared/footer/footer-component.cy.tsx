22279ac246188198edb8573100b1ed9658f1d194
Benjamin Weare <ben.weare12@gmail.com>
2024-07-08 10:40:27 +0100

    Final attempt at pulling correct test versions

--- src/components/shared/footer/footer-component.cy.tsx
+++ src/components/shared/footer/footer-component.cy.tsx
@@ -6,4 +6,18 @@ describe('<FooterComponent />', () => {
     it('should render without any props', () => {
         cy.mount(<FooterComponent />);
     });
+
+    it('should render with the correct version number', () => {
+        cy.mount(<FooterComponent />);
+
+        const package_version =
+            VITE_APP_VERSION ??
+            Cypress.env('VITE_APP_VERSION') ??
+            process.env?.VITE_APP_VERSION;
+
+        cy.get('[data-cy="version-number"]').should(
+            'contain.text',
+            `v${package_version}`
+        );
+    });
 });
