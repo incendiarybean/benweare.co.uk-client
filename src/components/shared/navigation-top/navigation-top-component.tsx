2c04314b45c36124a27e7391ad020fa782a160b9
Benjamin Weare <ben.weare12@gmail.com>
2024-07-08 11:58:50 +0100

    import meta

--- src/components/shared/navigation-top/navigation-top-component.tsx
+++ src/components/shared/navigation-top/navigation-top-component.tsx
@@ -21,7 +21,7 @@ const NavigationBar = () => {
                     </a>
                     <span className='ml-3 text-xs leading-5 font-medium text-sky-600 dark:text-sky-400 bg-sky-400/30 dark:bg-sky-400/10 rounded-full py-1 px-3 items-center'>
                         <strong className='font-semibold'>
-                            v{VITE_APP_VERSION}
+                            v{import.meta.env.VITE_APP_VERSION}
                         </strong>
                     </span>
                 </div>
