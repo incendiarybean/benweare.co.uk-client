a97033fa75d9d68ffaf472bcb8517c1b3eba4e4d
Benjamin Weare <ben.weare12@gmail.com>
2024-07-22 16:14:57 +0100

    Updated cacheing to avoid docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigpaths from 'vite-tsconfig-paths';
@@ -8,50 +8,68 @@ export default defineConfig({
     plugins: [
         react(),
         tsconfigpaths(),
-        // VitePWA({
-        //     injectRegister: 'auto',
-        //     workbox: {
-        //         globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,webp}'],
-        //         runtimeCaching: [
-        //             {
-        //                 urlPattern: ({ url: { pathname } }) =>
-        //                     pathname.includes('/api/news'),
-        //                 handler: 'CacheFirst',
-        //                 options: {
-        //                     cacheName: 'api-cache',
-        //                     expiration: {
-        //                         maxEntries: 10,
-        //                         maxAgeSeconds: 60 * 60 * 24 * 365,
-        //                     },
-        //                     cacheableResponse: {
-        //                         statuses: [0, 200],
-        //                     },
-        //                 },
-        //             },
-        //         ],
-        //     },
-        //     manifestFilename: 'manifest.json',
-        //     manifest: {
-        //         short_name: "Benjamin (Ben) Weare's web page",
-        //         name: "Benjamin (Ben) Weare's web page",
-        //         start_url: '.',
-        //         display: 'standalone',
-        //         theme_color: '#000000',
-        //         background_color: '#ffffff',
-        //         icons: [
-        //             {
-        //                 src: 'favicon.png',
-        //                 sizes: '32x32',
-        //                 type: 'png',
-        //             },
-        //             {
-        //                 src: 'favicon.ico',
-        //                 sizes: '32x32',
-        //                 type: 'image/x-icon',
-        //             },
-        //         ],
-        //     },
-        // }),
+        VitePWA({
+            injectRegister: 'auto',
+            workbox: {
+                navigateFallbackDenylist: [/.*\/api\/docs/],
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,webp}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ url: { pathname } }) =>
+                            pathname.includes('/api/news'),
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'api-cache',
+                            expiration: {
+                                maxEntries: 100,
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                    {
+                        urlPattern: ({ request }) =>
+                            request.destination === 'image',
+                        handler: 'StaleWhileRevalidate',
+                        options: {
+                            cacheName: 'image-cache',
+                            expiration: {
+                                maxEntries: 100,
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                ],
+            },
+            manifestFilename: 'manifest.json',
+            manifest: {
+                short_name: "Benjamin (Ben) Weare's web page",
+                name: "Benjamin (Ben) Weare's web page",
+                start_url: '.',
+                display: 'standalone',
+                theme_color: '#000000',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'favicon.png',
+                        sizes: '32x32',
+                        type: 'png',
+                    },
+                    {
+                        src: 'favicon.ico',
+                        sizes: '32x32',
+                        type: 'image/x-icon',
+                    },
+                ],
+            },
+            devOptions: {
+                enabled: true,
+                type: 'module',
+            },
+        }),
     ],
     server: {
         port: 3000,
