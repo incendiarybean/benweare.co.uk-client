ca7110ec28bf238862757c3842796da8dd87be07
Ben <66177972+incendiarybean@users.noreply.github.com>
2024-07-22 11:18:07 +0100

    FEATURE: Enabling PWA  (#67)
     Added first iteration of PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,58 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigpaths from 'vite-tsconfig-paths';
 import { version } from './package.json';
 
 export default defineConfig({
-    plugins: [react(), tsconfigpaths()],
+    plugins: [
+        react(),
+        tsconfigpaths(),
+        VitePWA({
+            injectRegister: 'auto',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,webp}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ url: { pathname } }) =>
+                            pathname.includes('/api/news'),
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'api-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365,
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                ],
+            },
+            manifestFilename: 'manifest.json',
+            manifest: {
+                short_name: "Benjamin (Ben) Weare's web page",
+                name: "Benjamin (Ben) Weare's web page",
+                start_url: '.',
+                display: 'standalone',
+                theme_color: '#000000',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'favicon.png',
+                        sizes: '32x32',
+                        type: 'png',
+                    },
+                    {
+                        src: 'favicon.ico',
+                        sizes: '32x32',
+                        type: 'image/x-icon',
+                    },
+                ],
+            },
+        }),
+    ],
     server: {
         port: 3000,
         proxy: {
