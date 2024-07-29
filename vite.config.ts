7d4d8994650ad7e6c87096d355ea09f799e55877
Benjamin Weare <ben.weare12@gmail.com>
2024-07-29 13:21:16 +0100

    Updated icons and PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -56,16 +56,33 @@ export default defineConfig({
                 background_color: '#ffffff',
                 icons: [
                     {
-                        src: 'favicon.png',
-                        sizes: '32x32',
-                        type: 'png',
-                    },
-                    {
-                        src: 'favicon.ico',
-                        sizes: '32x32',
+                        src: 'icons/favicon.ico',
+                        sizes: '48x48',
                         type: 'image/x-icon',
                     },
+                    {
+                        src: 'icons/favicon.png',
+                        sizes: '256x256',
+                        purpose: 'any',
+                        type: 'image/png',
+                    },
                 ],
+                screenshots: [
+                    {
+                        src: 'screenshots/screen-mobile.png',
+                        sizes: '390x844',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'screenshots/screen-desktop.png',
+                        sizes: '1280x720',
+                        form_factor: 'wide',
+                        type: 'image/png',
+                    },
+                ],
+            },
+            devOptions: {
+                enabled: true,
             },
         }),
     ],
