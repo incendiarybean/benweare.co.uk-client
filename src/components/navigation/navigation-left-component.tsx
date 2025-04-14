63fd3edcaf603fdcf93e61709911e03db3c2962b
Benjamin Weare <ben.weare12@gmail.com>
2025-04-14 18:51:59 +0100

    chore: left adjustment of sidebar

--- src/components/navigation/navigation-left-component.tsx
+++ src/components/navigation/navigation-left-component.tsx
@@ -6,7 +6,7 @@ const LeftNavigationBar = () => {
             data-cy='navigation-left'
             className='hidden md:flex min-w-64 w-64 justify-center'
         >
-            <div className='flex flex-col gap-2 mt-24 w-48 fixed top-0'>
+            <div className='flex flex-col gap-2 mt-24 -ml-4 w-auto fixed top-0'>
                 <GenerateDestinations />
             </div>
         </div>
